61566fe1d9ca8b33582a9cb0c1ef1486d45eba24
Borhan <brborhan70@gmail.com>
2025-04-04 04:31:56 +0600

    vite.config.js file update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,28 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+      },
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+      "/socket.io": {
+        target: "ws://localhost:5000",
+        ws: true,
+      },
+    },
+  },
 });
